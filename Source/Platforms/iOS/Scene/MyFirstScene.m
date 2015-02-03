2b98bd2d004b4fc1f0fe2086eb198ba35c16dcc9
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-02-03 11:22:09 +0200

    no message

--- Source/Platforms/iOS/Scene/MyFirstScene.m
+++ Source/Platforms/iOS/Scene/MyFirstScene.m
@@ -13,9 +13,11 @@
 -(id) init {
     if ((self = [super init]))
     {
+        [self setUserInteractionEnabled:YES];
+        
         // create and initialize a label
         CCLabelTTF* label = [CCLabelTTF labelWithString:@"Hello World"
-                                               fontName:@"Verdana-Bold"//@"Marker Felt"
+                                               fontName:@"Marker Felt"
                                                fontSize:64];
         
         // get the window (screen) size from CCDirector
@@ -24,7 +26,27 @@
         label.position = CGPointMake(size.width / 2, size.height / 2);
         //add the label as a child to this Layer
         [self addChild:label];
+        
+        self.colorRGBA = [CCColor colorWithCcColor4b:ccc4(20, 35, 20, 255)];
+
+        CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Settings@2x.png"];
+        
+        sprite.position = ccp(100, 100);
+        
+        [self addChild:sprite];
     }
+    
     return self;
 }
+
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    NSLog(@"Touch start");
+}
+
+- (void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    NSLog(@"Touch end");
+}
+
+
+
 @end
